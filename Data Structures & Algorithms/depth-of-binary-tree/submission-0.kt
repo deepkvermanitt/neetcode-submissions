f79461b37bec3c63a06ac900e8e4d86cc74f8b27
deepkvermanitt <deepkvermanitt@gmail.com>
2026-06-10 13:13:52 +0530

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.kt
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun maxDepth(root: TreeNode?): Int {
+        if (root ==null){
+            return 0
+        }
+        val left = maxDepth(root.left)
+        val right = maxDepth(root.right)
+        return maxOf(left,right)+1
+    }
+}
